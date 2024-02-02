f9251c009fedc78bac91c38c56ad66cc23759a05
Christopher Lane <clane19@student.cscc.edu>
2024-02-02 13:56:12 -0500

    Fill the test file

---
+++ 0/rectagle/tests/__init__.py
@@ -0,0 +1,108 @@
+import check50
+import re
+
+
+@check50.check()
+def exists():
+    """rectangle.py exists"""
+    check50.exists("rectangle.py")
+
+
+@check50.check(exists)
+def test5and3():
+    """input of 5 and 3 yeilds output of 26"""
+    output = (
+        check50.run("python3 rectangle.py")
+        .stdin("5", prompt=False)
+        .stdin("3", prompt=False)
+        .stdout()
+    )
+
+    # Extract number from stdout
+    match = re.findall(r"([.,]?(?:\d[.,]?)+)", output)
+    if match is None:
+        raise check50.Failure("Looks like your program didn't output a number!")
+    number = match[-1]
+
+    # Match correct number
+    if not re.match(r"^26$", number):
+        raise check50.Mismatch(
+            "26",
+            number,
+            help="Seems like your output might not be the right number!",
+        )
+
+
+@check50.check(exists)
+def test3and5():
+    """input of 3 and 5 yeilds output of 26"""
+    output = (
+        check50.run("python3 rectangle.py")
+        .stdin("3", prompt=False)
+        .stdin("5", prompt=False)
+        .stdout()
+    )
+
+    # Extract number from stdout
+    match = re.findall(r"([.,]?(?:\d[.,]?)+)", output)
+    if match is None:
+        raise check50.Failure("Looks like your program didn't output a number!")
+    number = match[-1]
+
+    # Match correct number
+    if not re.match(r"^26$", number):
+        raise check50.Mismatch(
+            "26",
+            number,
+            help="Seems like your output might not be the right number!",
+        )
+
+
+@check50.check(exists)
+def test1and1():
+    """input of 1 and 1 yeilds output of 4"""
+    output = (
+        check50.run("python3 rectangle.py")
+        .stdin("1", prompt=False)
+        .stdin("1", prompt=False)
+        .stdout()
+    )
+
+    # Extract number from stdout
+    match = re.findall(r"([.,]?(?:\d[.,]?)+)", output)
+    if match is None:
+        raise check50.Failure("Looks like your program didn't output a number!")
+    number = match[-1]
+
+    # Match correct number
+    if not re.match(r"^4$", number):
+        raise check50.Mismatch(
+            "4",
+            number,
+            help="Seems like your output might not be the right number!",
+        )
+
+
+@check50.check(exists)
+def test23and45():
+    """input of 23 and 45 yeilds output of 96"""
+    output = (
+        check50.run("python3 rectangle.py")
+        .stdin("3", prompt=False)
+        .stdin("5", prompt=False)
+        .stdout()
+    )
+
+    # Extract number from stdout
+    match = re.findall(r"([.,]?(?:\d[.,]?)+)", output)
+    if match is None:
+        raise check50.Failure("Looks like your program didn't output a number!")
+    number = match[-1]
+
+    # Match correct number
+    if not re.match(r"^96$", number):
+        raise check50.Mismatch(
+            "96",
+            number,
+            help="Seems like your output might not be the right number!",
+        )
