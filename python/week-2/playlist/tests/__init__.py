1edb53344dc25487dc740d0d027577a594c21e11
Makaze <christopherslane.work@gmail.com>
2024-03-03 11:41:36 -0500

    Try no prompt

--- python/week-2/playlist/tests/__init__.py
+++ python/week-2/playlist/tests/__init__.py
@@ -77,51 +77,49 @@ def test_one_song_output():
 1. Soothsayer by Backethead
 """
 
-    check50.run("python3 playlist.py").stdin("7", prompt=True).stdin(
-        "Soothsayer", prompt=True
-    ).stdin("Buckethead", prompt=True).stdout(output, output, regex=True).exit()
+    check50.run("python3 playlist.py").stdin("7", prompt=False).stdin(
+        "Soothsayer", prompt=False
+    ).stdin("Buckethead", prompt=False).stdout(output, output, regex=True).exit()
 
 
 @check50.check(test_one_song)
 def test_seven_song_output():
     """playlist.py outputs a multi-song playlist with correct format"""
 
-    output = """
-1. Soothsayer by Backethead
+    output = """1. Soothsayer by Backethead
 2. Everybody Wants to Rule the World by Tears for Fears
 3. Everything in Its Right Place by Radiohead
 4. Visa fran Utanmyra by Jan Johansson
 5. Road to Nowhere by Talking Heads
 6. Orbaum by Grip on the News
-7. Fool me twice. by kuiper
-"""
+7. Fool me twice. by kuiper"""
 
-    check50.run("python3 playlist.py").stdin("7", prompt=True).stdin(
-        "Soothsayer", prompt=True
-    ).stdin("Buckethead", prompt=True).stdin(
-        "Everybody Wants to Rule the World", prompt=True
+    check50.run("python3 playlist.py").stdin("7", prompt=False).stdin(
+        "Soothsayer", prompt=False
+    ).stdin("Buckethead", prompt=False).stdin(
+        "Everybody Wants to Rule the World", prompt=False
     ).stdin(
-        "Tears for Fears", prompt=True
+        "Tears for Fears", prompt=False
     ).stdin(
-        "Everything in Its Right Place", prompt=True
+        "Everything in Its Right Place", prompt=False
     ).stdin(
-        "Radiohead", prompt=True
+        "Radiohead", prompt=False
     ).stdin(
-        "Visa fran Utanmyra", prompt=True
+        "Visa fran Utanmyra", prompt=False
     ).stdin(
-        "Jan Johansson", prompt=True
+        "Jan Johansson", prompt=False
     ).stdin(
-        "Road to Nowhere", prompt=True
+        "Road to Nowhere", prompt=False
     ).stdin(
-        "Talking Heads", prompt=True
+        "Talking Heads", prompt=False
     ).stdin(
-        "Orbaum", prompt=True
+        "Orbaum", prompt=False
     ).stdin(
-        "Grip on the News", prompt=True
+        "Grip on the News", prompt=False
     ).stdin(
-        "Fool me twice.", prompt=True
+        "Fool me twice.", prompt=False
     ).stdin(
-        "kuiper", prompt=True
+        "kuiper", prompt=False
     ).stdout(
         output, output, regex=True
     ).exit()
