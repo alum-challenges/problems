9ce20687d1ea4acdf9b9c212eefb3234de865935
Makaze <christopherslane.work@gmail.com>
2024-07-17 11:19:12 -0400

    Remove input

--- fpdf/__init__.py
+++ fpdf/__init__.py
@@ -6,6 +6,4 @@ def test_range_level_1():
     """Print fpdf version"""
 
     # With random.seed(0) in testing.py, 6 + 6 is expected output from randint and randrange with range of 0–9
-    check50.run("pip3 freeze | grep fpdf").stdin(level, prompt=False).stdout(
-        "Debugging", "Debugging", regex=True
-    )
+    check50.run("pip3 freeze | grep fpdf").stdout("Debugging", "Debugging", regex=True)
