ad67a818693c26c137ba2c98a2cedbf2b02c89e1
Dana-f559 <bogdanafarace559@gmail.com>
2025-02-26 22:36:49 +0200

    tests improved

--- python/week-2/dots/tests/__init__.py
+++ python/week-2/dots/tests/__init__.py
@@ -1,28 +1,49 @@
 import check50
 
+
 @check50.check()
 def exists():
-    """dots.py exists"""
-    check50.exists("dots.py")
-    check50.include("tests.py")
+   """dots.py exists"""
+   check50.exists("dots.py")
+   check50.include("tests.py")
+
 
 @check50.check(exists)
 def test_decode_small():
-    """decode successfully returns a simple decoded string."""
-    exit = check50.run("pytest tests.py -k 'test_decode_small'").exit()
-    if exit == 1:
-        raise check50.Failure(f"Decode doesn't  successfully decodes a simple string.")
+   """decode successfully returns a simple one word decoded string."""
+   # decode successfully returns
+   exit = check50.run("pytest tests.py -k 'test_decode_small'").exit()
+   if exit == 1:
+       raise check50.Failure(f"Decode doesn't successfully decodes a simple string.")
+
 
 @check50.check(exists)
 def test_decode_small_numbers():
-    """decode successfully returns a simple decoded string with numbers."""
-    exit = check50.run("pytest tests.py -k 'test_decode_numbers'").exit()
-    if exit == 1:
-        raise check50.Failure(f"Decode doesn't  successfully decodes a simple string with numbers.")
+   """decode successfully returns a simple decoded string with numbers."""
+   exit = check50.run("pytest tests.py -k 'test_decode_numbers'").exit()
+   if exit == 1:
+       raise check50.Failure(f"Decode doesn't successfully decodes a simple string with numbers.")
+
+
+@check50.check(exists)
+def test_decode_repeated_chars():
+   """decode successfully returns a simple decoded with repeated characters."""
+   exit = check50.run("pytest tests.py -k 'test_decode_double'").exit()
+   if exit == 1:
+       raise check50.Failure(f"Decode doesn't successfully decodes a simple string with repeated characters.")
+
 
 @check50.check(exists)
 def test_decode_small_non_alphanumerical():
-    """decode successfully returns a simple decoded string with non alphanumarical charachters."""
-    exit = check50.run("pytest tests.py -k 'test_decode_non_alphanumeric'").exit()
-    if exit == 1:
-        raise check50.Failure(f"Decode doesn't  successfully decodes a simple string with non alphanumarical charachters.")
+   """decode successfully returns a simple decoded string with non alphanumeric characters."""
+   exit = check50.run("pytest tests.py -k 'test_decode_non_alphanumeric'").exit()
+   if exit == 1:
+       raise check50.Failure(f"Decode doesn't successfully decodes a simple string with non alphanumeric characters.")
+
+
+@check50.check(exists)
+def test_decode_advanced():
+   """decode successfully returns an advanced decoded string."""
+   exit = check50.run("pytest tests.py -k 'test_decode_advanced'").exit()
+   if exit == 1:
+       raise check50.Failure(f"Decode doesn't successfully decodes an advanced string.")
